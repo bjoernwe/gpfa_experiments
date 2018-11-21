3488331bf9d1af288a816788466bcbeccaa63126
Björn <bjoern.weghenkel@gmail.com>
2018-11-21 11:46:48 +0100

    axes

--- src/experiments_proxy/plot_angles_to_p1.py
+++ src/experiments_proxy/plot_angles_to_p1.py
@@ -72,14 +72,14 @@ def main():
                 # random
                 values_random = results_random[min_principal_angle][dataset].values * ( 180. / np.pi)
                 d, _ = values_random.shape
-                plt.errorbar(x=range(1,d+1), y=np.mean(values_random, axis=1), yerr=np.std(values_random, axis=1), color='silver', ls='--', dashes=(5,2), zorder=0)
+                plt.errorbar(x=search_range_p[alg], y=np.mean(values_random, axis=1), yerr=np.std(values_random, axis=1), color='silver', ls='--', dashes=(5,2), zorder=0)
 
             for min_principal_angle in [False, True]:
 
                 # angles
                 values = results_angle[alg][min_principal_angle][dataset].values * ( 180. / np.pi)
                 d, _ = values.shape
-                plt.errorbar(x=range(1,d+1), y=np.mean(values, axis=1), yerr=np.std(values, axis=1), color='green' if min_principal_angle else 'blue', zorder=10)
+                plt.errorbar(x=search_range_p[alg], y=np.mean(values, axis=1), yerr=np.std(values, axis=1), color='green' if min_principal_angle else 'blue', zorder=10)
                 xlim_max = 4.5 if alg is eb.Algorithms.GPFA2 else 6.5
                 plt.xlim(.5, xlim_max)
                 #plt.ylim(-.2, np.pi/2+.2)
@@ -93,7 +93,6 @@ def main():
                     plt.gca().set_yticklabels([])
                 if (alg is eb.Algorithms.ForeCA and idx >= 4) or idx >= 12:
                     plt.xlabel('p')
-                    plt.gca().set_xticklabels(search_range_p[alg])
                 else:
                     plt.gca().set_xticklabels([])
 
